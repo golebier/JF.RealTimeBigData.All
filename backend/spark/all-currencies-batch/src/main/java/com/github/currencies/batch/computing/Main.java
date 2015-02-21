72eaf19942d7521c09aa1d69135ad039d0c12219
Gra <golebier@gmail.com>
2015-02-21 15:12:59 +0100

    Map to pairs for Average, Sum, Count, added.

--- backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
+++ backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
@@ -9,10 +9,16 @@ import java.net.UnknownHostException;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.StorageLevels;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import scala.Tuple2;
+import scala.Tuple3;
+import scala.Tuple4;
 
 import com.github.currencies.holder.CurrenciesHolder;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
@@ -26,16 +32,26 @@ public class Main {
     public static void main( String[] args ) throws UnknownHostException, IOException, GeoIp2Exception {
 	    CHECK_USEGE(args);
 	    JavaStreamingContext ssc = PREPARE_SPARK_CONTEXT();
-
 	    JavaReceiverInputDStream<String> lines = ssc.socketTextStream(
 	            args[0], Integer.parseInt(args[1]), StorageLevels.MEMORY_AND_DISK_SER);
 	    JavaDStream<CurrenciesHolder> jsons = lines.flatMap(CurrenciesHolder.PREPARE_LINES());
+	    // count 
+	    // sum 
+	    // avr 
+	    JavaPairDStream<Tuple3<String, String, String>, Tuple4<Double, Double, Double, CurrenciesHolder>> pairs
+	    	= jsons.mapToPair(new PairFunction<CurrenciesHolder
+	    			, Tuple3<String, String, String>, Tuple4<Double, Double, Double, CurrenciesHolder>>() {
+				private static final long serialVersionUID = -5800347417661881676L;
+				public Tuple2<Tuple3<String, String, String>, Tuple4<Double, Double, Double, CurrenciesHolder>>
+					call(CurrenciesHolder data) {
+					return null;
+				}});
 	    
 		// TODO save ES instead
 //		computeAndSave(withoutNulls);
 //		DatabaseReaderWrapper.close();
 	    //jsons.print();
-	    JavaDStream<Long> count = jsons.count();
+	    JavaDStream<Long> count = pairs.count();
 	    count.print();
 
 	    ssc.start();
