977efc872a35ceff8a152975378321828d2ca770
Gra <golebier@gmail.com>
2015-02-21 13:52:39 +0100

    Cleanup before final realtime implementation.

--- backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
+++ backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
@@ -73,71 +73,15 @@ public class Main {
     public static void main( String[] args ) throws UnknownHostException, IOException, GeoIp2Exception {
 	    CHECK_USEGE(args);
 	    JavaSparkContext sc = PREPARE_SPARK_CONTEXT();
-	    String logFilePath = args[0];
 
-	    // USE Spark to generate those jsons ;)
-	    
-	    // TODO {"userId": "134256", "currencyFrom": "EUR", "currencyTo": "GBP", "amountSell": 1000, "amountBuy": 747.10
-	    //       , "rate": 0.7471, "timePlaced" : "14-JAN-15 10:27:44", "originatingCountry" : "FR"}
-
-	    // TODO Stream from Flume
-	    JavaRDD<String> currencies = sc.textFile(logFilePath);
-		JavaRDD<CurrenciesHolder> parts = currencies.flatMap(CurrenciesHolder.FLAT_MAP_JSONS());
-		JavaRDD<CurrenciesHolder> withoutNulls = parts.filter(CurrenciesHolder.FILTER_NOT_NULL());
-
-		// TEST generate N jsons per T ime with random fields
-//		JavaRDD<CurrenciesHolder> newLines = withoutNulls.map(new Function<CurrenciesHolder, CurrenciesHolder>() {
-//			private static final long serialVersionUID = -6170423912286687076L;
-//			public CurrenciesHolder call(CurrenciesHolder s) {
-//				  return s;
-//		    }}
-//        );
-
-	    JavaRDD<String> logLines = sc.textFile("1line.log");
-		int NUM_SAMPLES = 216000000;// -- 60 000 per sec by hour, 1440000000 -- 400 000 per sec by hour
-    	double renge = Math.sqrt(NUM_SAMPLES)+1;
-	    // get a length that will give a base for join
-    	JavaRDD<Integer> parallelized0 = sc.parallelize(makeRange(0, (int)renge)).filter(FILTER());
-    	JavaRDD<Integer> parallelized1 = sc.parallelize(makeRange(0, (int)renge)).filter(FILTER());
-    	JavaPairRDD<Integer, Integer> cartesianed = parallelized0.cartesian(parallelized1);
-		JavaPairRDD<Tuple2<Integer, Integer>, String> paired = cartesianed.cartesian(logLines);
-		JavaRDD<String> randomized = paired.map(new Function<Tuple2<Tuple2<Integer, Integer>, String>, String>() {
-			private static final long serialVersionUID = 8910642060539381814L;
-			@Override
-			public String call(Tuple2<Tuple2<Integer, Integer>, String> data) throws Exception {
-				// TODO make it random
-				CurrenciesHolder ch = CurrenciesHolder.parseFromCurrencyJson(data._2());
-				String randomized = ch.ramdomize();
-				return data._2();
-			}
-			
-		});
-
-		long count = randomized.count();
-		System.out.println("------------------- "+count+", "+NUM_SAMPLES);
-		
+		// TODO save ES instead
 //		computeAndSave(withoutNulls);
 
 		sc.stop();
 //		DatabaseReaderWrapper.close();
     }
 
-	private static Function<Integer, Boolean> FILTER() {
-		return new Function<Integer, Boolean>() {
-			private static final long serialVersionUID = -7482464778722345646L;
-			public Boolean call(Integer i) {
-			    return true;// Math.random()
-			}
-		};
-	}
-    
-	private static List<Integer> makeRange(int i, int size) {
-		List<Integer> out = new ArrayList<Integer>();
-		for (; i < size; ++i) {
-			out.add((int)Math.random());
-		}
-		return out;
-	}
+
     // DONE: Message Consumption & Message Processor in one, Spark is ok with that, Test Flume ...
 
 	private static void CHECK_USEGE(String[] args) {
