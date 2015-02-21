54e9ab404840ab658f97e716099411dd75087f0f
Gra <golebier@gmail.com>
2015-02-21 16:23:05 +0100

    Save calculations to elasticsearch --- streaming part/realtime. TODO corrections with batch part.

--- backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
+++ backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
@@ -6,8 +6,11 @@ package com.github.currencies.batch.computing;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
@@ -17,6 +20,7 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
 import scala.Tuple2;
 import scala.Tuple3;
@@ -62,13 +66,46 @@ public class Main {
 			        return new Tuple3<Double, Double, Double>(a._1()+b._1(), a._2()+b._2(), (a._3()+b._3())/2);
 			    }
 		    });
-		// TODO save ES instead
-	    JavaDStream<Long> countreduced = reduced.count();
-	    JavaDStream<Long> count = lines.count();
-	    // show amount of jsons per a sec
-	    count.print();
-	    countreduced.print();
-	    reduced.print();
+
+	    // TODO batch corrections
+	    // TODO UI
+
+	    // Ok, add more of the CurrenciesHolder fields = use CurrenciesHolder
+		// save ES instead -- prepare data
+	    JavaDStream<Map<String, Object>> javaRDD = reduced.map(
+        		new Function<Tuple2<Tuple3<String, String, String>, Tuple3<Double, Double, Double>>, Map<String, Object>>() {
+			private static final long serialVersionUID = -6509311005347136809L;
+			@Override
+	  		public Map<String, Object>
+				call(Tuple2<Tuple3<String, String, String>, Tuple3<Double, Double, Double>> data) throws Exception {
+	  			Map<String, Object> line = new HashMap<String, Object>();
+	  			line.put("originatingCountry", data._1()._1());
+	  			line.put("currencyFrom", data._1()._2());
+	  			line.put("currencyTo", data._1()._3());
+	  			line.put("amountSell", data._2()._1().toString());
+	  			line.put("amountBuy", data._2()._2().toString());
+	  			line.put("rate", data._2()._3().toString());
+	  			return line;
+	  		}
+	  	});
+	    // save ES instead -- save data
+	    javaRDD.foreach(new Function<JavaRDD<Map<String, Object>>, Void>() {
+			private static final long serialVersionUID = 1714172195345175479L;
+			@Override
+			public Void call(JavaRDD<Map<String, Object>> rdd) throws Exception {
+				//JavaEsSpark.saveToEs(rdd, "currency_batch_spark_dev/currency_batch_spark");
+				JavaEsSpark.saveToEs(rdd, "currency_streaming_spark_dev/currency_streaming_spark");
+				return (Void) null;
+			}
+		});
+
+	    // Debug ;)
+//	    JavaDStream<Long> countreduced = reduced.count();
+//	    JavaDStream<Long> count = lines.count();
+//	    // show amount of jsons per a sec
+//	    count.print();
+//	    countreduced.print();
+//	    reduced.print();
 
 	    ssc.start();
 	    ssc.awaitTermination();
