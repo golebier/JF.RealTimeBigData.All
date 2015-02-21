f947c103c5f89da6e431591ebadf56f1609368bc
Gra <golebier@gmail.com>
2015-02-21 16:00:11 +0100

    App name fixed.

--- backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
+++ backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
@@ -82,7 +82,7 @@ public class Main {
 	}
 
 	private static JavaStreamingContext PREPARE_SPARK_CONTEXT() {
-		SparkConf conf = new SparkConf().setAppName("Batch Analytics");
+		SparkConf conf = new SparkConf().setAppName("Real time currencyF.");
 		conf.set("es.nodes", "elasticsearch-1:9200");
 		conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
 		conf.set("spark.kryo.registrator", "com.github.currencies.registrator.CurrenciesRegistrator");
