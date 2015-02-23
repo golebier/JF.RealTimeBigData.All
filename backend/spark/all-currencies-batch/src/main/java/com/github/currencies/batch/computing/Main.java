95d7a3bf8e9061fe88daef3a38738f1bced50037
Gra <golebier@gmail.com>
2015-02-23 08:26:29 +0100

    Cleanup.

--- backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
+++ backend/spark/all-currencies-batch/src/main/java/com/github/currencies/batch/computing/Main.java
@@ -15,7 +15,7 @@ import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
@@ -34,9 +34,9 @@ import com.maxmind.geoip2.exception.GeoIp2Exception;
  * @author gra
  */
 public class Main {
-    public static void main( String[] args ) throws UnknownHostException, IOException, GeoIp2Exception {
+    public static void main(String[] args) throws UnknownHostException, IOException, GeoIp2Exception {
 	    CHECK_USEGE(args);
-	    JavaStreamingContext ssc = PREPARE_SPARK_CONTEXT();
+	    JavaStreamingContext ssc = PREPARE_SPARK_CONTEXT(args);
 	    JavaReceiverInputDStream<String> lines = ssc.socketTextStream(
 	            args[0], Integer.parseInt(args[1]), StorageLevels.MEMORY_AND_DISK_SER);
 	    JavaDStream<CurrenciesHolder> jsonsMapped = lines.flatMap(CurrenciesHolder.PREPARE_LINES());
@@ -47,16 +47,17 @@ public class Main {
 				  return (null!=s)&&s.noAnyNull();
 		    }
         });
-	    JavaPairDStream<Tuple3<String, String, String>, CurrenciesHolder> pairs
-	    	= jsons.mapToPair(new PairFunction<CurrenciesHolder
+
+	    JavaPairDStream<Tuple3<String, String, String>, CurrenciesHolder> pairs = jsons
+	    		.mapToPair(new PairFunction<CurrenciesHolder
 	    			, Tuple3<String, String, String>, CurrenciesHolder>() {
 				private static final long serialVersionUID = -5800347417661881676L;
 				public Tuple2<Tuple3<String, String, String>, CurrenciesHolder>
 					call(CurrenciesHolder data) {
 					return data.prepareMapToPairs();
-				}});
-	    JavaPairDStream<Tuple3<String, String, String>, CurrenciesHolder> reduced
-	    	= pairs.reduceByKey(new Function2<
+		}})
+//	    JavaPairDStream<Tuple3<String, String, String>, CurrenciesHolder> reduced = pairs
+	    .reduceByKey(new Function2<
 	    			CurrenciesHolder, CurrenciesHolder, CurrenciesHolder>() {
 				private static final long serialVersionUID = -5793498283912220473L;
 				@Override
@@ -67,68 +68,67 @@ public class Main {
 	    			a.avrRate(b.getRate());
 			        return a;
 			    }
-		    });
+		    });//.updateStateByKey(updateFunc);
 
 	    // TODO batch corrections                     |
 	    // TODO compress distributed implementations  | --- those two I'll add in the future, this is good idea to correct real time results with batch results, but not for now.
 	    // TODO UI
+	    // TODO add flume configuration for distributed configuration
 
 	    // Ok, add more of the CurrenciesHolder fields = use CurrenciesHolder
-		// save ES instead -- prepare data
-	    JavaDStream<Map<String, Object>> javaRDD = reduced.map(
-        		new Function<Tuple2<Tuple3<String, String, String>, CurrenciesHolder>, Map<String, Object>>() {
+		// save ES with prepared data
+	    pairs.map(new Function<Tuple2<Tuple3<String, String, String>, CurrenciesHolder>, Map<String, Object>>() {
 			private static final long serialVersionUID = -6509311005347136809L;
 			@Override
 	  		public Map<String, Object>
 				call(Tuple2<Tuple3<String, String, String>, CurrenciesHolder> data) throws Exception {
 	  			Map<String, Object> line = new HashMap<String, Object>();
+	  			CurrenciesHolder ch = data._2();
 	  			// add all fields from CurrenciesHolder
-	  			line.put("originatingCountry", data._1()._1());
-	  			line.put("currencyFrom", data._1()._2());
-	  			line.put("currencyTo", data._1()._3());
+	  			line.put("userId", ch.getUserId());
+
+	  			line.put("originatingCountry", ch.getOriginatingCountry());
+	  			line.put("currencyFrom", ch.getCurrencyFrom());
+	  			line.put("currencyTo", ch.getCurrencyTo());
 	  			// or even the new type
-	  			line.put("amountSell", data._2().getAmountSell().toString());
-	  			line.put("amountBuy", data._2().getAmountBuy().toString());
-	  			line.put("rate", data._2().getRate().toString());
+	  			line.put("amountSell",ch.getAmountSell().toString());
+	  			line.put("amountBuy", ch.getAmountBuy().toString());
+	  			line.put("rate", ch.getRate().toString());
+
+	  			line.put("timePlaced", ch.getTimePlaced().getMillis());
+	  			line.put("timestamp", ch.getTimePlaced().getMillis());
 	  			return line;
 	  		}
-	  	});
-	    // save ES instead -- save data
-	    javaRDD.foreach(new Function<JavaRDD<Map<String, Object>>, Void>() {
+	  	}).foreachRDD(new Function<JavaRDD<Map<String, Object>>, Void>() {
 			private static final long serialVersionUID = 1714172195345175479L;
 			@Override
 			public Void call(JavaRDD<Map<String, Object>> rdd) throws Exception {
+				// this one in batch ;)
 				//JavaEsSpark.saveToEs(rdd, "currency_batch_spark_dev/currency_batch_spark");
 				JavaEsSpark.saveToEs(rdd, "currency_streaming_spark_dev/currency_streaming_spark");
 				return (Void) null;
 			}
 		});
-
-	    // Debug ;)
-//	    JavaDStream<Long> countreduced = reduced.count();
-//	    JavaDStream<Long> count = lines.count();
-//	    // show amount of jsons per a sec
+	    // Debug ;) hm, 60k in 1 - 3s but on 1pc(8x3GHz, em: 4GB)  
+//	    JavaDStream<Long> count = pairs.count();
 //	    count.print();
-//	    countreduced.print();
-//	    reduced.print();
 
 	    ssc.start();
 	    ssc.awaitTermination();
     }
 
 	private static void CHECK_USEGE(String[] args) {
-		if (2 != args.length) {
-		      System.out.println("Error: expected: <hostname> <port>.");
+		if (3 != args.length) {
+		      System.out.println("Error: expected: <hostname> <port> <elasticsearch:port>.");
 		      System.exit(-1);
         }
 	}
 
-	private static JavaStreamingContext PREPARE_SPARK_CONTEXT() {
+	private static JavaStreamingContext PREPARE_SPARK_CONTEXT(String[] args) {
 		SparkConf conf = new SparkConf().setAppName("Real time currencyF.");
-		// TODO add ES URl as a args[3] 
-		conf.set("es.nodes", "localhost:9200");
+		conf.set("es.nodes", args[2]);
 		conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
 		conf.set("spark.kryo.registrator", "com.github.currencies.registrator.CurrenciesRegistrator");
-		return new JavaStreamingContext(conf, Durations.seconds(1));
+		return new JavaStreamingContext(conf, new Duration(1000));// Durations.seconds(1));
 	}
 }
